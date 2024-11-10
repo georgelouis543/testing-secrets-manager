e91d9a6b67fa01ecfec55b028d06af4a7d62e935
georgelouis <george.louis@meltwater.com>
2024-11-10 19:08:03 +0530

    other changes

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
 
+from dotenv import load_dotenv
+import os
+
+
 app = FastAPI()
+
+
+load_dotenv()
+
+TESTING_ENV_VARIABLE = os.getenv("TESTING_ENV_VARIABLE")
 
 
 @app.get("/")
 async def root():
     return {
         "message": "Testing Secrets Manager for MWFeeds. This is a Demo Project."
+    }
+
+
+@app.get("/test-access")
+async def testing_env_variable():
+    testing_variable = TESTING_ENV_VARIABLE or None
+    return {
+        "access_variable": testing_variable
     }
